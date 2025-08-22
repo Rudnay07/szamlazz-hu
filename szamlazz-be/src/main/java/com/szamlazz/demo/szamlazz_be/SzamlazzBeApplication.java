2b3f9f5ccbcaf80e1cf84cb37f6e952cd44afe66
Rudnay Roland <rudnayroland@gmail.com>
2025-08-22 19:14:02 +0200

    defa

---
+++ szamlazz-be/src/main/java/com/szamlazz/demo/szamlazz_be/SzamlazzBeApplication.java
@@ -0,0 +1,13 @@
+package com.szamlazz.demo.szamlazz_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzamlazzBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzamlazzBeApplication.class, args);
+	}
+
+}
---
+++ szamlazz-be/src/test/java/com/szamlazz/demo/szamlazz_be/SzamlazzBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szamlazz.demo.szamlazz_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzamlazzBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
